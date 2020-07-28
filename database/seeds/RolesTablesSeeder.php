d9a225a2992daad66c8eb580af76c9e9d37faf6d
unknown <vitorfleite@live.com>
2020-07-28 17:04:15 -0300

    commit

--- database/seeds/RolesTablesSeeder.php
+++ database/seeds/RolesTablesSeeder.php
@@ -14,28 +14,28 @@ class RolesTablesSeeder extends Seeder
     public function run()
     {
         $super_adminRole = new Role;
-        $super_adminRole->display_name = 'Owner';
+        $super_adminRole->display_name = 'Proprietário';
         $super_adminRole->external_id = Uuid::uuid4();
         $super_adminRole->name = 'owner';
         $super_adminRole->description = 'Owner';
         $super_adminRole->save();
 
         $adminRole = new Role;
-        $adminRole->display_name = 'Administrator';
+        $adminRole->display_name = 'Administrador';
         $adminRole->external_id = Uuid::uuid4();
         $adminRole->name = 'administrator';
         $adminRole->description = 'System Administrator';
         $adminRole->save();
 
         $editorRole = new Role;
-        $editorRole->display_name = 'Manager';
+        $editorRole->display_name = 'Gerente';
         $editorRole->external_id = Uuid::uuid4();
         $editorRole->name = 'manager';
         $editorRole->description = 'System Manager';
         $editorRole->save();
 
         $employeeRole = new Role;
-        $employeeRole->display_name = 'Employee';
+        $employeeRole->display_name = 'Empregado';
         $employeeRole->external_id = Uuid::uuid4();
         $employeeRole->name = 'employee';
         $employeeRole->description = 'Employee';
