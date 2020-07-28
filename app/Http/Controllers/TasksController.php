0717d0dd542501ea75de4e6c3e18ec816f423db7
unknown <vitorfleite@live.com>
2020-07-28 20:17:28 -0300

    commit

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -157,8 +157,9 @@ class TasksController extends Controller
         }
 
         //Hack to make dropzone js work, as it only called with AJAX and not form submit
-        return response()->json(['task_external_id' => 'crm/'. $task->external_id]);
-        //return redirect()->route("tasks.show", $insertedExternalId);
+        //return response()->json(['task_external_id' => $task->external_id, 'project_external_id' => $project ? $project->external_id : null]);
+        //return redirect()->route("tasks.show", $request, $insertedExternalId);
+       return redirect('crm/tasks/'.$task->external_id);
     }
 
     private function upload($image, $task)
